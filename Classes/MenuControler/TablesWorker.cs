b617da2f02fbd127d92b51f46498d1107c90bc62
Данил Кузнецов <danilkuzn2004@gmail.com>
2024-12-25 18:52:43 +0700

    Revert "Получение доступа из БД, вроде работает удаление"
    This reverts commit 329a13bbdc2ac1cbbb7feea390a60589d9b3b761.

--- Classes/MenuControler/TablesWorker.cs
+++ Classes/MenuControler/TablesWorker.cs
@@ -5,7 +5,6 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
 
 namespace ShippingCompany.Classes.MenuControler
@@ -28,40 +27,22 @@ namespace ShippingCompany.Classes.MenuControler
         /// <param name="tableName">Название таблицы базы данных.</param>
         public static void LoadTableFromDatabase(MainWindow mainWindow, string tableName)
         {
-            if (string.IsNullOrEmpty(tableName))
-            {
-                MessageBox.Show("Имя таблицы не может быть пустым!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
+            // Очистка текущего содержимого
+            mainWindow.MainContent.Children.Clear();
 
-            try
-            {
-                // Очистка текущего содержимого
-                mainWindow.MainContent.Children.Clear();
+            // Выполнение SQL-запроса
+            string query = $"SELECT * FROM {tableName};";
+            DataTable dataTable = DatabaseManager.Instance.ExecuteQuery(query);
 
-                // Выполнение SQL-запроса
-                string query = $"SELECT * FROM {tableName};";
-                DataTable dataTable = DatabaseManager.Instance.ExecuteQuery(query);
-
-                // Преобразование DataTable в DataGrid
-                DataGrid dataGrid = new DataGrid
-                {
-                    ItemsSource = dataTable.DefaultView
-                };
-
-                // Добавление DataGrid в MainWindow
-                mainWindow.MainContent.Children.Add(dataGrid);
-            }
-            catch (Npgsql.PostgresException ex) when (ex.SqlState == "42P01")
+            // Преобразование DataTable в DataGrid
+            DataGrid dataGrid = new DataGrid
             {
-                MessageBox.Show($"Таблица '{tableName}' не существует в базе данных.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Ошибка при загрузке таблицы: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
+                ItemsSource = dataTable.DefaultView
+            };
+
+            // Добавление DataGrid в MainWindow
+            mainWindow.MainContent.Children.Add(dataGrid);
         }
-
 
         /// <summary>
         /// Метод для создания и отображения структуры таблицы из БД в MainWindow.
